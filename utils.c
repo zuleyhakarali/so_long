99659f827fc8614ed1dafe26c0cfb14972cefbc5
Züleyha Karali <zuleyha.karali04@gmail.com>
2025-11-06 08:21:16 +0300

    xpm

--- utils.c
+++ utils.c
@@ -33,9 +33,12 @@ static void sec_placement(t_long *game, char c, int j, int i)
     if (c == '1')
         mlx_put_image_to_window(game->mlx, game->window, game->iwall,
                                 j * SIZE, i * SIZE);
-    else
+    else if (c == '0')
         mlx_put_image_to_window(game->mlx, game->window, game->ifloor,
                                 j * SIZE, i * SIZE);
+    else
+        mlx_put_image_to_window(game->mlx, game->window, game->iplayer,
+                                        j * SIZE, i * SIZE);
 }
 
 void placement(t_long *game)
@@ -47,21 +50,21 @@ void placement(t_long *game)
     while (game->map[i])
     {
         j = 0;
-        while (game->map[i][j++])
+        while (game->map[i][j])
         {
             if (game->map[i][j] == '0')
                 sec_placement(game, '0', j, i);
             if (game->map[i][j] == '1')
                 sec_placement(game, '1', j, i);
             if (game->map[i][j] == 'P')
-                mlx_put_image_to_window(game->mlx, game->window, game->iplayer,
-                                        j * SIZE, i * SIZE);
+                sec_placement(game, 'P', j, i);
             if (game->map[i][j] == 'C')
                 mlx_put_image_to_window(game->mlx, game->window, game->icoll,
                                         j * SIZE, i * SIZE);
             if (game->map[i][j] == 'E')
                 mlx_put_image_to_window(game->mlx, game->window, game->iexit,
                                         j * SIZE, i * SIZE);
+            j++;
         }
         i++;
     }
